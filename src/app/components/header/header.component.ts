8e5237c6153b0f42b9672fde6553aaf3056c43eb
Tshepiso28 <tshepisostudent@gmail.com>
2025-04-07 12:39:39 +0200

    I was learning Angular directives

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  a: number = 0;
+  b: number = 0;
+
+  items = [
+    {id: 1, name: 'Banana'},
+    {id: 2, name: 'Apple'},
+    {id: 3, name: 'Orange'},
+    {id: 4, name: 'Peach'}
+  ]
+
+  isActive: boolean = false;
+
+  toggleActive(){
+    this.isActive = !this.isActive;
+  }
+  fSize = "50px";
+  tAlign = " "
+  bgColor = "red";
+
+  updateStyle(){
+    this.fSize = "120px";
+    this.bgColor = "blue";
+    this.tAlign = "center"
+
+  }
+
+}
