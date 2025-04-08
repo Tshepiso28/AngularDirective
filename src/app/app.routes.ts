fdf3adb2db77d196dc7de2ce72832ff66706ed9f
Tshepiso28 <tshepisostudent@gmail.com>
2025-04-08 11:10:20 +0200

    Created the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FormsComponent } from './components/forms/forms.component';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'form', component: FormsComponent},
+    {path: 'head', component: HeaderComponent}
+];
---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+
+}
