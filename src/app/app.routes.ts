1ffd1c74ed3d1bfbff3f6567a12ac1ad6283aa02
Tshepiso28 <tshepisostudent@gmail.com>
2025-04-11 10:46:20 +0200

    Created a profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FormsComponent } from './components/forms/forms.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
     {path: 'form', component: FormsComponent},
-    {path: 'head', component: HeaderComponent}
+    {path: 'head', component: HeaderComponent},
+    {path: 'profile', component: ProfileComponent}
 ];
